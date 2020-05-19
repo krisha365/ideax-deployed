0dff09083c1df24fa9bab9460537d09fd3a4a697
phineas6725 <30693448+Rahul6725@users.noreply.github.com>
2020-05-19 16:05:01 +0530

    Add files via upload

--- ideaX/settings.py
+++ ideaX/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'backend.apps.BackendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'ideaX.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ideaX',
+        'USER': 'postgres',
+        'PASSWORD': 'toor',
+        'HOST': 'localhost'
     }
 }
 
