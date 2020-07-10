846d8a590df632361f34fc762bdb116965d515b9
phineas6725 <30693448+Rahul6725@users.noreply.github.com>
2020-07-10 20:46:38 +0530

    Add files via upload

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'azuresite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ideaX',
+        'USER': 'postgres',
+        'PASSWORD': 'toor',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+        
     }
 }
 
