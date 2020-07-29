522de3452f368f7923964783503839afe6a1465d
kunjdepani <36526763+kunjdepani@users.noreply.github.com>
2020-07-29 21:33:34 +0530

    Add files via upload

--- ideax/ideax/settings.py
+++ ideax/ideax/settings.py
@@ -127,3 +127,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "ideaxlive@gmail.com"
+EMAIL_HOST_PASSWORD = "Live1ideax"
